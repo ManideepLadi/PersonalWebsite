52657de70e1199db907e62c45bb26bddf0cf42e7
manideep <manideep.ladi@zemosolabs.com>
2020-08-25 20:47:53 +0530

    Updates

---
+++ precache-manifest.f3eabb521208acc1d54ef387bd34301e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad64afe76ce66853d77c3f849122fa4",
+    "url": "/PersonalWebsite/static/media/manideepSketch.fad64afe.jpeg"
+  },
+  {
+    "revision": "2e985b43df9fb49ad811",
+    "url": "/PersonalWebsite/static/js/runtime~main.53ae6336.js"
+  },
+  {
+    "revision": "8897d4f430fcdc0afc97",
+    "url": "/PersonalWebsite/static/js/main.adef14b7.chunk.js"
+  },
+  {
+    "revision": "e494278789dfd999b392",
+    "url": "/PersonalWebsite/static/js/2.c4a4627e.chunk.js"
+  },
+  {
+    "revision": "8897d4f430fcdc0afc97",
+    "url": "/PersonalWebsite/static/css/main.2eb502a4.chunk.css"
+  },
+  {
+    "revision": "6cb683f78e41be9510a0885397916f4c",
+    "url": "/PersonalWebsite/index.html"
+  }
+];
